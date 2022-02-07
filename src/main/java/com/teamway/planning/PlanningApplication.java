0ef3cc7f95fdce1c04ea001c19e42c1bfc4c6f47
Rafael_Silva <rafael.ds81@gmail.com>
2022-02-07 22:54:19 +0000

    Added eureka

--- src/main/java/com/teamway/planning/PlanningApplication.java
+++ src/main/java/com/teamway/planning/PlanningApplication.java
@@ -2,8 +2,10 @@ package com.teamway.planning;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class PlanningApplication {
 
 	public static void main(String[] args) {
